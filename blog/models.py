7b6c36ba33eced8b299d4e3fe64befe71a6ecc9a
Shakhov Pavel <shakhov.pavel@rt.ru>
2025-12-29 00:07:59 +0300

    init

---
+++ blog/models.py
@@ -0,0 +1,29 @@
+from django.db.models import Model, CharField, SlugField, TextField, DateTimeField, Index, TextChoices, ForeignKey
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(Model):
+    class Status(TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
+    title = CharField(max_length=250)
+    slug = SlugField(max_length=250, unique=True)
+    author = ForeignKey(User,on_delete=models.CASCADE, related_name="blog_posts")
+    body = TextField()
+
+    publish = DateTimeField(default=timezone.now)
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+    status = CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
