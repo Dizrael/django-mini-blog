aa292f4f1f62811812f7b8e41e0e05dbebf35ffc
Shakhov Pavel <shakhov.pavel@rt.ru>
2025-12-29 15:05:47 +0300

    change posts path url

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,30 @@
 from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
 
 def post_list(request: HttpRequest):
-    posts = Post.published.all()
+    all_posts = Post.published.all()
+    paginator = Paginator(all_posts, 3)
+    page_num = request.GET.get("page", 1)
+    try:
+        posts = paginator.page(page_num)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_detail(request: HttpRequest, id: int):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-
+def post_detail(request: HttpRequest, year: int, month: int, day: int, slug: str):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=slug,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
     return render(request, "blog/post/detail.html", {"post": post})
