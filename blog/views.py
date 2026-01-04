d4d0b87c69bc7981d298e9fc83e3fbb3f10ebadf
Dizrael <pashamyl999@gmail.com>
2026-01-04 22:38:37 +0300

    add email sent functionality

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,11 @@ from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
+from django.conf import settings
 
 from .models import Post
+from .forms import EmailPostForm
 
 
 class PostListView(ListView):
@@ -36,3 +39,28 @@ def post_detail(request: HttpRequest, year: int, month: int, day: int, slug: str
         publish__day=day,
     )
     return render(request, "blog/post/detail.html", {"post": post})
+
+def post_share(request, post_id):
+    # извлекаем пост по id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == "POST":
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}'s ({cd['email']}) comments: {cd['comments']}"
+            to = cd['to']
+            if isinstance(to, str):
+                if ',' not in to:
+                    to = [to]
+                else:
+                    to = [to_s.strip() for to_s in to.split(',')]
+            send_mail(subject, message, settings.EMAIL_HOST_USER, to)
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, "blog/post/share.html", {"post": post, "form": form, 'sent': sent})
--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+# Конфигурация сервера электронной почта
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "your_email"
+EMAIL_HOST_PASSWORD = "pass"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
