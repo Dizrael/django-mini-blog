2be4d81edf9c89630c3ea302cbb72fc2f5f81590
Dizrael <pashamyl999@gmail.com>
2026-01-06 18:57:35 +0300

    add custom tags

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,35 @@
+import markdown
+import nh3
+from django import template
+from django.utils.safestring import mark_safe
+
+from ..models import Post
+from django.db.models import Count
+
+register = template.Library()
+
+
+@register.simple_tag
+def total_posts() -> int:
+    return Post.published.count()
+
+
+@register.inclusion_tag("blog/post/latest_posts.html")
+def show_latest_posts(count=5) -> dict:
+    latest_posts = Post.published.order_by("-publish")[:count]
+    return {"latest_posts": latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5) -> list[Post]:
+    return (
+        Post.published.annotate(total_comments=Count("comments"))
+        .exclude(total_comments=0)
+        .order_by("-total_comments")[:count]
+    )
+
+
+@register.filter(name="markdown")
+def markdown_format(text: str) -> str:
+    clean_text = nh3.clean(markdown.markdown(text))
+    return mark_safe(clean_text)
