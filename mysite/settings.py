d4fde146217f74b27e2e8e63a78e914f96b37d54
Dizrael <pashamyl999@gmail.com>
2026-01-18 01:33:58 +0300

    add django-environ db init

--- mysite/settings.py
+++ mysite/settings.py
@@ -8,12 +8,21 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(BASE_DIR / '.env')
+
+env = environ.Env(
+    PG_DB=(str, 'blog'),
+    PG_USER=(str, 'blog'),
+    PG_PASS=(str, '')
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -78,10 +87,7 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
+    "default": env.db()
 }
 
 
